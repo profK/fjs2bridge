1a695572bfedac95d7299e7cc43e0d0484c96fed
Jeff Kessleman <jeffpk@worldwizards.net>
2020-07-16 16:30:00 -0400

    Generates class properties

--- FVTTBridge/Bindings/AudioHelper.cs
+++ FVTTBridge/Bindings/AudioHelper.cs
@@ -9,8 +9,14 @@ namespace FVTTBridge.Bindings
     [External]
     [Namespace(false)]    
     public class AudioHelper{
-    
+       public dynamic _analyserStreams;
+       public dynamic _audioContext;
+       public dynamic pending;
+       public dynamic locked;
+       public dynamic _analyserInterval;
+       public dynamic sounds;
+       public dynamic _fftArray;
+
     }
         
 }
-        
